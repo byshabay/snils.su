a031a1e03439368f9c4505689c1b5e8c1aece2fc
Alyona Shabaykina <shabbaykina@mail.ru>
2021-07-30 19:33:37 +0300

    Add desktop header

---
+++ script.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    /*BURGER*/
+    $('.header__burger').click(function(event){
+        $('.header__burger, .header__list, .header__info').toggleClass('active');
+    });
+    /*ACTIVE MENU LINKS*/
+   var url = $(location).attr('href');
+   $.each($('.header__bottom_list li a'), function(){
+        if(this.href == url){
+            $(this).addClass('active_link');
+        }
+   });
+});
